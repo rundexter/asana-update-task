ce8673595ae89b74f55d895e16f81f16ec223b5d
Daniel Ilkovich <daniel+github@betaworks.com>
2016-03-08 20:02:23 -0500

    more specific input processing

--- index.js
+++ index.js
@@ -12,21 +12,30 @@ module.exports = {
      */
     run: function(step, dexter) {
         var credentials    = dexter.provider('asana').credentials('access_token')
-          , inputs         = _.omit(step.inputs(), 'task')
           , tasks          = step.input('task')
           , promises       = []
         ;
 
-        tasks.each(function(task) {
+        tasks.each(function(task, idx) {
 
             var url     = 'https://app.asana.com/api/1.0/tasks/' + encodeURIComponent(task)
+              , data    = {}
               , request = req.put(url)
-                              .type('json')
-                              .set('Authorization', 'Bearer '+credentials)
-                              .send( { data: inputs } )
+                            .type('json')
+                            .set('Authorization', 'Bearer '+credentials)
             ;
 
-            console.log(url, inputs);
+            _.each(['assignee', 'completed', 'due_on', 'name', 'notes', 'projects']
+              , function(key) {
+                  setIfExists(data, key, step.input(key)[idx] || step.input(key).first());
+              });
+
+            //normalize projects to an array
+            if(data.projects) {
+                data.projects = _.isArray(data.projects) ? data.projects : [ data.projects ];
+            }
+
+            request = request.send({ data: data });
 
             promises.push(
                 promisify(request, 'end', 'body.data')
